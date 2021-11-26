79849bd26c79356f17f5ca230e2f5e4195d4108a
Roberto Martínez Medina <robcs@MacBook-Pro-de-Roberto.local>
2021-11-26 12:44:43 -0600

    Added: Navigation component configuration

---
+++ app/src/main/java/cs/builder/app/example/moderncleanarchitecture/app/feature/main/favorites/FavoritesFragment.kt
@@ -0,0 +1,19 @@
+package cs.builder.app.example.moderncleanarchitecture.app.feature.main.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cs.builder.app.example.moderncleanarchitecture.R
+
+/** */
+class FavoritesFragment : Fragment() {
+
+    /** */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_favorites, container, false)
+
+}
